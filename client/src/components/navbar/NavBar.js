e0a1d05c1117dd982e4d4efb577e50029a821a07
Mechkour Hafida <79710349+Hafida-mh@users.noreply.github.com>
2023-02-01 10:17:53 +0100

    Update NavBar.js

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -27,13 +27,9 @@ export default function NavBar(props) {
   }
   return (
     <div className='navContainer'>
-
-
-
       <div className='navContent'>
         <div className='navItem'>
           <div className='logo'> <img src={Logo} /></div>
-
           <div className='items'>
             <div className='item'> <a href={props.linkaccueil}> Accueil  </a></div>
             <div className='item'> <a href={props.linkdecouvrir}> Découvrir </a>  </div>
@@ -41,38 +37,29 @@ export default function NavBar(props) {
             <div className='item'> <a href={props.linkfooter}>  Contact </a> </div>
           </div>
         </div>
-
         <div className='navButton'>
           <div className='inscription'>  <Link to="/signup" className='linkToClient'>  <div> Inscription </div>  </Link></div>
           <div className='connexion'>  <Link to="/signin" className='linkToConnexion'>  <div>  Connexion  </div> </Link></div>
           <div className='burgerButton' onClick={() => showResponsiveNav() }> <div>  <img src={BurgerMenu} className="burgerMenuSvg"/>  </div></div>
         </div>
       </div>
-     
       { show &&
         <div className='navResponsive'>
           <div className='navResponsiveContent'>
-          <div className='logo responsive'> <img src={Logo} /></div>
+            <div className='logo responsive'> <img src={Logo} /></div>
             <div className='itemsResponsive'>
               <div className='item'> Accueil </div>
               <div className='item'> Découvrir </div>
               <div className='item'> Abonnement </div>
               <div className='item'> Contact </div>
             </div>
-
-
             <div className='buttonResponsive'>  
-
             <div className='inscription'> <div> Inscription </div> </div>
-          <div className='connexion'> <div>  Connexion  </div></div>
-            </div>
+            <div className='connexion'> <div>  Connexion  </div></div>
           </div>
-        </div>
+          </div>
+       </div>
       }
-
-
     </div>
-
-
   )
 }
