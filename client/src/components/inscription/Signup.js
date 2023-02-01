ae0dc85053417f65d81bf3ef0c7014ac2520bfa8
Mechkour Hafida <79710349+Hafida-mh@users.noreply.github.com>
2023-02-01 10:16:10 +0100

    Update Signup.js

--- client/src/components/inscription/Signup.js
+++ client/src/components/inscription/Signup.js
@@ -16,7 +16,6 @@ export default function Signup() {
     return (
         <div className='signup'>
             {  /* <div className='navbarcontainer'> <NavBar />  </div>*/}
-
             <div className='sectionLogo'>
                 <div className='logoCedre'>
                     <Link to="/" >  <img src={Logocedre} /> </Link>
@@ -27,57 +26,36 @@ export default function Signup() {
                 <div className='formContent'>
                     <form className='ff'>
                         <div className='formInner'>
-
                             <div className='titleForm'>  Je m'inscris </div>
                             <div className='formItems'>
                                 <div className='formItemsTitle'> Nom complet </div>
                                 <div> <input className='inputSignup' placeholder='Nom complet' /> </div>
                             </div>
-
                             <div className='formItems'>
                                 <div className='formItemsTitle'> Pseudo </div>
                                 <div> <input className='inputSignup' placeholder='Pseudo' /> </div>
                             </div>
-
                             <div className='formInputs'>
-
                                 <div className='formInputsItems'>
-
                                     <div> Email</div>
                                     <div> <input className='input' placeholder='Email' /> </div>
                                 </div>
-
-
                                 <div className='formInputsItems'>
-
                                     <div> Confirmer email </div>
                                     <div> <input className='input' placeholder='Confirmer Email' /> </div>
                                 </div>
-
                             </div>
 
-
                             <div className='formInputs'>
-
                                 <div className='formInputsItems'>
-
                                     <div> Password </div>
                                     <div> <input className='input' placeholder='Password' /> </div>
                                 </div>
-
-
                                 <div className='formInputsItems'>
-
                                     <div> Confirmer password </div>
                                     <div> <input className='input' placeholder='Confirmer password' /> </div>
                                 </div>
-
                             </div>
-
-
-
-
-
                             <div className='captchaContainer'>     <ReCAPTCHA sitekey="Your client site key" className='captcha' />   </div>
                             <div className='condition'>
                                 <div className='conditionTitle'> Condition d'utilisation</div>
@@ -86,28 +64,16 @@ export default function Signup() {
                                     <div className='check'> J'ai lu et accepté les <span className='section'> conditions d'utilisation</span></div>
                                 </div>
                             </div>
-
-
-
-
                             <div className='formButton'>
                                 <button className='buttonSignup'> Je m'inscris </button>
                             </div>
-
-
                             <div className='link'> J'ai déja un compte </div>
                         </div>
-
                     </form>
-
-
                 </div>
             </div>
 
-
             <div className='footersignupcontainer'> <Footer /></div>
-
-
         </div>
     )
 }
