096417348039e29145a9b79dcfae73bb679529d4
Mechkour Hafida <79710349+Hafida-mh@users.noreply.github.com>
2023-02-01 10:01:34 +0100

    Update Footer.js

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -8,12 +8,8 @@ import Logofooter from '../../images/cedre/logo blanc_Plan de travail 1.png'
 export default function Footer() {
     return (
         <div className='footer'>
-
-
-            <div className='footerContent'>
-
+            <div className='footerContent'> 
                 <div className='footerContainer'>
-
                     <div className='listItemFooter'>
                         <div> Accueil </div>
                         <div> Découvrir </div>
@@ -21,33 +17,25 @@ export default function Footer() {
                         <div> Contact </div>
                         <div> Connexion </div>
                     </div>
-
                     <div className='logoFooter'> <img src={Logofooter} /></div>
-
                     <div className='newsletter'>
                         <div className='textNewsletter'> Inscrivez-vous à notre newsletter </div>
                         <form>
                             <div className='formContainerFooter'>
-
                                 <div> <input className='inputNewsletter' placeholder='Entrer email' /> </div>
                                 <div> <button className='button'> ok </button> </div>
-
                             </div>
                         </form>
-
                         <div className='socialMedia'>
                             <div className='socialMediaItem'> <img src={Facebook} /> </div>
                             <div className='socialMediaItem'> <img src={Instagram} /> </div>
                             <div className='socialMediaItem'> <img src={Linkdin} /> </div>
                             <div className='socialMediaItem'> <img src={Youtube} /> </div>
                         </div>
-                        
                     </div>
-                </div>
+                   </div>
                 <div className='signature'> © 2021 Cédre QCM. Tous les droits sont réservés. </div>
-
             </div>
-
         </div>
     )
 }
